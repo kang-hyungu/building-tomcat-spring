1e594a7492096663a7e2494609be99f8ba86051a
KangHyunGu <hkkang@woowahan.com>
2021-07-20 17:31:07 +0900

    feat: request, response 처리 추가

--- app/src/main/java/nextstep/jwp/JwpApplication.java
+++ app/src/main/java/nextstep/jwp/JwpApplication.java
@@ -3,7 +3,8 @@ package nextstep.jwp;
 public class JwpApplication {
 
     public static void main(String[] args) {
-        final WebServer webServer = new WebServer(8080, 1000);
+        int port = WebServer.defaultPortIfNull(args);
+        final WebServer webServer = new WebServer(port);
         webServer.run();
     }
 }
