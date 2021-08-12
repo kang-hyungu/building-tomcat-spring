a8ce207c4d039032772611dc245b2fd8142fe7f8
KangHyunGu <hkkang@woowahan.com>
2021-08-12 13:20:23 +0900

    feat: 톰캣 구현하기 미션 초기 셋팅

--- app/src/main/java/nextstep/jwp/JwpApplication.java
+++ app/src/main/java/nextstep/jwp/JwpApplication.java
@@ -1,14 +1,44 @@
 package nextstep.jwp;
 
-import nextstep.jwp.mvc.RequestMapping;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.scan.StandardJarScanFilter;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 public class JwpApplication {
 
-    public static void main(String[] args) {
-        final int port = WebServer.defaultPortIfNull(args);
-        final RequestMapping requestMapping = new RequestMapping();
+    private static final Logger log = LoggerFactory.getLogger(JwpApplication.class);
 
-        final WebServer webServer = new WebServer(port, requestMapping);
-        webServer.run();
+    public static void main(String[] args) throws Exception {
+        final int port = WebServer.defaultPortIfNull(args);
+        final Tomcat tomcat = new Tomcat();
+        tomcat.setPort(port);
+
+        final String path = JwpApplication.class.getClassLoader().getResource("static").getPath();
+        final File docBase = new File(path);
+        final Context context = tomcat.addWebapp(null, "/", docBase.getAbsolutePath());
+        log.info("docBase : {}", docBase.getAbsolutePath());
+
+        skipBindOnInit(tomcat);
+        skipTldScan(context);
+
+        tomcat.start();
+    }
+
+    private static void skipBindOnInit(Tomcat tomcat) {
+        final Connector connector = tomcat.getConnector();
+        connector.setProperty("bindOnInit", "false");
+    }
+
+    private static void skipTldScan(Context context) {
+        final StandardJarScanner scanner = (StandardJarScanner) context.getJarScanner();
+        final StandardJarScanFilter filter = (StandardJarScanFilter) scanner.getJarScanFilter();
+        filter.setDefaultTldScan(false);
+        filter.setDefaultPluggabilityScan(false);
     }
 }
