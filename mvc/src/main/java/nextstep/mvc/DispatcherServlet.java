2d2d9ae46f13faf9ed6e2f8cbcb042b93be11ba6
KangHyunGu <hkkang@woowahan.com>
2021-08-20 13:07:37 +0900

    feat: 임시저장

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.JspView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,8 +18,6 @@ import java.util.Objects;
 public class DispatcherServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
-
-    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
     private final List<HandlerMapping> handlerMappings = new ArrayList<>();
     private final List<HandlerAdapter> handlerAdapters = new ArrayList<>();
@@ -56,12 +55,12 @@ public class DispatcherServlet extends HttpServlet {
                 .filter(Objects::nonNull)
                 .findFirst()
                 .map(Controller.class::cast)
-                .orElse(null);
+                .orElseThrow(RuntimeException::new);
     }
 
     private void move(String viewName, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
+        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
             return;
         }
 
--- mvc/src/main/java/nextstep/mvc/view/JspView.java
+++ mvc/src/main/java/nextstep/mvc/view/JspView.java
@@ -7,27 +7,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class JspView implements View {
 
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
 
-    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
+    public static final String REDIRECT_PREFIX = "redirect:";
 
     private final String viewName;
 
     public JspView(String viewName) {
-        if (viewName == null) {
-            throw new NullPointerException("viewName is null. 이동할 URL을 입력하세요.");
-        }
-        this.viewName = viewName;
+        this.viewName = Objects.requireNonNull(viewName, "viewName is null. 이동할 URL을 입력하세요.");
     }
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         log.debug("ViewName : {}", viewName);
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
             return;
         }
 
